04813dc51bc7b117db06262da45f54826187cefd
shubham yadav <shubhamdv19@gmail.com>
2020-08-02 21:15:20 +0530

    name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: ` Shubham Yadav Personal Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Shubham Yadav`,
+      summary: ` `,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `shubhamdsm`,
     },
   },
   plugins: [
